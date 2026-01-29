108e147f517dace306b6bade103df5c6c846e6c0
장준태 <91938434+jangjuntae@users.noreply.github.com>
2026-01-29 22:28:31 +0900

    [level 1] Title: 폰켓몬, Time: 3.04 ms, Memory: 92.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        answer = Math.min(set.size(), nums.length / 2);
+        
+        return answer;
+    }
+}
