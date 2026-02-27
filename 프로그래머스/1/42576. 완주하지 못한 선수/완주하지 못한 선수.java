599cdd7c3a2fb82b8270610de6c73f7a69328515
장준태 <91938434+jangjuntae@users.noreply.github.com>
2026-02-28 00:51:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.83 ms, Memory: 96.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        for(String s : participant){
+            hm.put(s, hm.getOrDefault(s, 0) + 1);
+        }
+        
+        for(String s : completion){
+            hm.put(s, hm.get(s) - 1);
+        }
+        
+        for(String key : hm.keySet()){
+            if(hm.get(key) != 0){
+                answer = key;
+            }
+        }
+        
+        return answer;
+    }
+}
