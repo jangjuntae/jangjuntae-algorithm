687a53469cddb7751527da0f749ac5127dab1edc
장준태 <91938434+jangjuntae@users.noreply.github.com>
2026-03-05 20:55:12 +0900

    [Silver I] Title: 경로 찾기, Time: 156 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -19,7 +19,7 @@ public class Main {
         for(int k = 0; k < n; k++){
             for(int i = 0; i < n; i++){
                 for(int j = 0; j < n; j++){
-                    if (!(arr[i][k] != 1 || arr[k][j] != 1)) {
+                    if (arr[i][k] == 1 && arr[k][j] == 1) {
                         arr[i][j] = 1;
                     }
                 }
