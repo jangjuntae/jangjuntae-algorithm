6f6e20122c0679f94052b279d8607f80eb5379af
장준태 <91938434+jangjuntae@users.noreply.github.com>
2026-05-13 11:15:24 +0900

    [D3] Title: 24시간, Time: 96 ms, Memory: 25,728 KB -BaekjoonHub

---
+++ SWEA/D3/12368. 24시간/24시간.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			int result = (n+m) % 24;
+			
+			System.out.println("#" + t +" " + result);
+		}
+	}
+}
