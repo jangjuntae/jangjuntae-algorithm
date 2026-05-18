dbc957a05f1aa30b348058a40628b1b111454069
장준태 <91938434+jangjuntae@users.noreply.github.com>
2026-05-18 11:00:02 +0900

    [D3] Title: 통나무 자르기, Time: 82 ms, Memory: 25,728 KB -BaekjoonHub

---
+++ SWEA/D3/14692. 통나무 자르기/README.md
@@ -0,0 +1,15 @@
+# [D3] 통나무 자르기 - 14692 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYJW0g-qlO8DFASv) 
+
+### 성능 요약
+
+메모리: 25,728 KB, 시간: 82 ms, 코드길이: 484 Bytes
+
+### 제출 일자
+
+2026-05-18 10:59
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/14692. 통나무 자르기/통나무 자르기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <= T; t++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			if(n % 2 == 0) {
+				System.out.println("#" + t + " Alice");
+			}
+			else {
+				System.out.println("#" + t + " Bob");
+			}
+		}
+	}
+}
