c103456f297e5ef790921cd9830ea1b7eb5aa70d
장준태 <91938434+jangjuntae@users.noreply.github.com>
2026-07-01 17:16:54 +0900

    [D1] Title: 자릿수 더하기, Time: 7 ms, Memory: 13,376 KB -BaekjoonHub

---
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        String str = br.readLine();
+        
+        int sum = 0;
+        
+        for(int i = 0; i < str.length(); i++) {
+        	sum += str.charAt(i) - '0';
+        }
+        
+        System.out.println(sum);
+    }
+}
